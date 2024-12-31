8dbcd7b73afbe9181b2d0cf52da9c669398788fe
fabasoad <fabasoad@gmail.com>
2024-12-31 14:46:54 +0900

    Update

--- src/setup-dependencies.sh
+++ src/setup-dependencies.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env sh
 
+_update() {
+  for arg in "$@"; do
+    if [ "$arg" = "false" ]; then
+      if [ "${RUNNER_OS}" = "Linux" ]; then
+        if [ -f "/etc/alpine-release" ]; then
+          apk update
+        else
+          apt-get update
+        fi
+      fi
+      exit 0
+    fi
+  done
+}
+
 _setup_dependencies_for_realpath() {
   if [ "${RUNNER_OS}" = "Linux" ]; then
     if [ -f "/etc/alpine-release" ]; then
@@ -62,6 +77,8 @@ main() {
   maven_installed="${3}"
   curl_installed="${4}"
   tar_installed="${5}"
+
+  _update "$@"
 
   if [ "${realpath_installed}" = "false" ]; then
     _setup_dependencies_for_realpath
