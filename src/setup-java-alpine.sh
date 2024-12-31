1402fe65e9c962facec3e93b232b9e9cf7258914
fabasoad <fabasoad@gmail.com>
2024-12-31 09:11:51 +0900

    Install java on Alpine

--- src/setup-java-alpine.sh
+++ src/setup-java-alpine.sh
@@ -2,7 +2,14 @@
 
 main() {
   if [ -f "/etc/alpine-release" ]; then
-    apk add --no-cache openjdk21 --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
+    apk add wget
+    wget -P /etc/apk/keys/ https://cdn.azul.com/public_keys/alpine-signing@azul.com-5d5dc44c.rsa.pub
+    echo "https://repos.azul.com/zulu/alpine" | tee -a /etc/apk/repositories
+    apk add zulu21-jdk
+
+    java_home="/usr/lib/jvm/zulu21-ca"
+    echo "JAVA_HOME=${java_home}" >> "$GITHUB_ENV"
+    echo "${java_home}/bin" >> "$GITHUB_PATH"
   fi
 }
 
