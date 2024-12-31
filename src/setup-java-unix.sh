bc45c63475483de8f9adec05854c9b3ee8e226fa
fabasoad <fabasoad@gmail.com>
2024-12-31 14:52:15 +0900

    Update

--- src/setup-java-unix.sh
+++ src/setup-java-unix.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
 main() {
-  if [ "${RUNNER_OS}" = "Windows" ]; then
+  if [ "${RUNNER_OS}" != "Linux" ]; then
     exit 0
   fi
 
   java_version="${1}"
-  if [ "${RUNNER_OS}" = "macOS" ]; then
-    brew install "openjdk@${java_version}"
-    java_home="/opt/homebrew/opt/openjdk@21"
-  elif [ "${RUNNER_OS}" = "Linux" ]; then
+  if [ "${RUNNER_OS}" = "Linux" ]; then
     os=$(grep "^ID=" "/etc/os-release" | cut -d '=' -f 2)
     if [ "${os}" = "alpine" ]; then
       apk add wget
@@ -20,7 +17,9 @@ main() {
       java_home="/usr/lib/jvm/zulu${java_version}-ca"
     else
       apt install openjdk-${java_version}-jdk -y
-      java_home="/usr/lib/jvm/java-21-openjdk-amd64"
+      java_home="/usr/lib/jvm/java-${java_version}-openjdk-amd64"
+      ls -la /usr/lib/jvm
+      ls -la "${java_home}"
     fi
   fi
 
