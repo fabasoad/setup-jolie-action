52071815d59250ab9595834c208d8571c93a3a3c
fabasoad <fabasoad@gmail.com>
2024-12-31 15:17:06 +0900

    Fix getting latest release

--- src/get-latest-release.sh
+++ src/get-latest-release.sh
@@ -8,8 +8,9 @@ main() {
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${token}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/${repo}/releases/latest")
-  echo "${version}"
+    "https://api.github.com/repos/${repo}/releases/latest" \
+    | jq -r '.tag_name | sub("^v"; "")')
+  echo "version=${version}" >> "$GITHUB_OUTPUT"
 }
 
 main "$@"
