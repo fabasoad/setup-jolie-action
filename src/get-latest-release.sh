8f8cb1d568f8dcd260f25a5606bc011e8a4ddcf9
Eugene Fabizhevskyi <yevhen.fabizhevskyi@woven.toyota>
2025-07-20 17:28:41 +0900

    test: add which jolie for debugging

--- src/get-latest-release.sh
+++ src/get-latest-release.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+SCRIPT_PATH=$(realpath "$0")
+SRC_DIR_PATH=$(dirname "$SCRIPT_PATH")
+LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
+
+. "${LIB_DIR_PATH}/logging.sh"
+
 main() {
   repo="${1}"
   token="${2}"
@@ -10,6 +16,7 @@ main() {
     -H "X-GitHub-Api-Version: 2022-11-28" \
     "https://api.github.com/repos/${repo}/releases/latest" \
     | jq -r '.tag_name | sub("^v"; "")')
+  log_info "Successfully retrieved ${GITHUB_SERVER_URL}/${repo} latest version: ${version}"
   echo "version=${version}" >> "$GITHUB_OUTPUT"
 }
 
